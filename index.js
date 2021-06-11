432f4fd7133a2c02bbfa48154c7160b993527c38
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-06-11 11:48:39 -0400

    closer to fine

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 // Template Literal for HTML
-const html =`<!DOCTYPE html>
+const mainHTML =`<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -30,57 +30,7 @@ const html =`<!DOCTYPE html>
         </div>
       </div>
 
-      <!-- Card Deck for team members -->
-      <div class="container">
-        <div class="row">
-          <div class="team-cards col-12 d-flex flex-row flex-wrap justify-content-center">
-
-          <!-- Manager card -->
-          <div class="card w-25 bg-light m-3 shadow rounded">
-            <div class="card-header text-light">
-              <h2 class="name">{manager.name}</h2>
-              <h3 class="title"><i class="fas fa-mug-hot mr-1"></i>{manager.role}</h3>
-            </div>
-            <div class="card-body">
-              <ul class="list-group list-group-flush border">
-                <li class="list-group-item">ID: {manager.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:{manager.email}">{manager.email}</a></li>
-                <li class="list-group-item">Office Phone: {manager.phone}</li>
-              </ul>
-            </div>
-          </div>    
-          
-      
-          <!-- Engineer Card -->
-          <div class="card w-25 bg-light m-3 shadow rounded">
-            <div class="card-header text-light">
-              <h2 class="name">{engineer.name}</h2>
-              <h3 class="title"><i class="fas fa-glasses mr-1"></i>{engineer.role}</h3>
-            </div>
-            <div class="card-body">
-              <ul class="list-group list-group-flush border">
-                <li class="list-group-item">ID: {engineer.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:{engineer.email}">{engineer.email}</a></li>
-                <li class="list-group-item">GitHub: <a href="https://github.com/{engineer.github}">{engineer.github}</a></li>
-              </ul>
-            </div>
-          </div>        
-
-          <!-- Intern Card -->
-          <div class="card w-25 bg-light m-3 shadow rounded">
-            <div class="card-header text-light">
-              <h2 class="name">{intern.name}</h2>
-              <h3 class="title"><i class="fas fa-user-graduate mr-1"></i>{intern.role}</h3>
-            </div>
-            <div class="card-body">
-              <ul class="list-group list-group-flush border">
-                <li class="list-group-item">ID: {intern.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:{intern.email}">{intern.email}</a></li>
-                <li class="list-group-item">School: {intern.school}</li>
-              </ul>
-            </div>
-          </div> 
-
+          ${cardDeck}
 
           </div>
         </div>
@@ -89,8 +39,73 @@ const html =`<!DOCTYPE html>
   </body>
 </html>`;
 
-// Empty array to add HTML team cards?
-const cardDeck = [];
+
+// Templates for each team member role: 
+
+const managerHTML = `           <!-- Manager card -->
+          <div class="card w-25 bg-light m-3 shadow rounded">
+            <div class="card-header text-light">
+              <h2 class="name">${manager.name}</h2>
+              <h3 class="title"><i class="fas fa-mug-hot mr-1"></i>${manager.role}</h3>
+            </div>
+            <div class="card-body">
+              <ul class="list-group list-group-flush border">
+                <li class="list-group-item">ID: ${manager.id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+                <li class="list-group-item">Office Phone: ${manager.phone}</li>
+              </ul>
+            </div>
+          </div>    
+          
+`;
+
+const engineerHTML = `          <!-- Engineer Card -->
+          <div class="card w-25 bg-light m-3 shadow rounded">
+            <div class="card-header text-light">
+              <h2 class="name">${engineer.name}</h2>
+              <h3 class="title"><i class="fas fa-glasses mr-1"></i>${engineer.role}</h3>
+            </div>
+            <div class="card-body">
+              <ul class="list-group list-group-flush border">
+                <li class="list-group-item">ID: ${engineer.id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/{engineer.github}">${engineer.github}</a></li>
+              </ul>
+            </div>
+          </div>        
+
+`;
+
+const internHTML = `          <!-- Intern Card -->
+          <div class="card w-25 bg-light m-3 shadow rounded">
+            <div class="card-header text-light">
+              <h2 class="name">${intern.name}</h2>
+              <h3 class="title"><i class="fas fa-user-graduate mr-1"></i>${intern.role}</h3>
+            </div>
+            <div class="card-body">
+              <ul class="list-group list-group-flush border">
+                <li class="list-group-item">ID: ${intern.id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+                <li class="list-group-item">School: ${intern.school}</li>
+              </ul>
+            </div>
+          </div> 
+
+`;
+
+
+
+// Team cards template, to add cards with new data for the mainHTML
+const cardDeck = `      <!-- Card Deck for team members -->
+      <div class="container">
+        <div class="row">
+          <div class="team-cards col-12 d-flex flex-row flex-wrap justify-content-center">
+ 
+    <!-- New cards added here-->
+
+`;
+
+
 
 // Need questions for inquirer:
 
@@ -238,7 +253,7 @@ function getInternInfo() {
 
 
 // Function to return HTML
-function writeHTML(team) {
+function writeHTML() {
   fs.writeFile('./dist/team.html', html, (err) => 
     err ? console.log(err) : console.log('You have successfully created your "team.html" file!')
     );
